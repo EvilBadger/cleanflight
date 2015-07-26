243f5561643ed13eacd33ede79bab453362c6ce1
geebles <giles.burgess@hp.com>
2015-07-26 18:01:25 +0100

    Fixed battery_unittest.cc for vbat changes

--- src/test/unit/battery_unittest.cc
+++ src/test/unit/battery_unittest.cc
@@ -42,6 +42,8 @@ TEST(BatteryTest, BatteryADCToVoltage)
     // batteryInit() reads a bunch of fields including vbatscale, so set up the config with useful initial values:
     batteryConfig_t batteryConfig = {
         .vbatscale = VBAT_SCALE_DEFAULT,
+        .vbatresdivval = VBAT_RESDIVVAL_DEFAULT,
+        .vbatresdivmultiplier = VBAT_RESDIVMULTIPLIER_DEFAULT,
         .vbatmaxcellvoltage = 43,
         .vbatmincellvoltage = 33,
         .vbatwarningcellvoltage = 35,
